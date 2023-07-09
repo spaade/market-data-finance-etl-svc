09c86aaaf77658732e8d53e569c82fa03b465088
Gustavo <gusttaa00@gmail.com>
2023-07-09 12:09:51 -0300

    Starting setting Job

--- src/main/java/yahoo/finance/etl/svc/jobs/YahooFinanceJob.java
+++ src/main/java/yahoo/finance/etl/svc/jobs/YahooFinanceJob.java
@@ -1,4 +1,41 @@
 package yahoo.finance.etl.svc.jobs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import yahoo.finance.etl.svc.dtos.StockDTO;
+import yahoo.finance.etl.svc.service.StocksService;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
 public class YahooFinanceJob {
+
+    private final static Logger log = LoggerFactory.getLogger(YahooFinanceJob.class);
+    private final StocksService stocksService;
+    private final WebClient webClient;
+
+    @Autowired
+    public YahooFinanceJob(StocksService stocksService, WebClient webClient) {
+        this.stocksService = stocksService;
+        this.webClient = webClient;
+    }
+
+    @Scheduled(initialDelay = 1L, fixedDelay = 1800000L)
+    public void execute() throws IOException {
+        List<StockDTO> stockDTOList = stocksService.findAllStocks().block();
+
+        assert stockDTOList != null;
+        for (StockDTO stockDTO : stockDTOList) {
+            Stock stock = YahooFinance.get(stockDTO.symbol());
+            stock.print();
+        }
+
+    }
 }
---
+++ src/main/java/yahoo/finance/etl/svc/jobs/YahooFinanceJobConstants.java
@@ -0,0 +1,6 @@
+package yahoo.finance.etl.svc.jobs;
+
+public class YahooFinanceJobConstants {
+
+    public static final String YAHOO_FINANCE_URL = "https://finance.yahoo.com/";
+}
